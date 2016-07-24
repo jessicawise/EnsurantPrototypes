d6eb3c7fc32f721c5bc0e3876966c03a4ba49242
Jessica Wise <jessica.lee.wise@gmail.com>
2016-07-24 12:58:56 -0400

    Added content and styling to About Us page.

--- app/components/aboutpage.js
+++ app/components/aboutpage.js
@@ -5,8 +5,45 @@ export default class AboutPage extends React.Component {
     return (
         <div className="inheritHeight">
           <div className="row inheritHeight content">
-            <div className="col-md-4 mission-div div-height">.col-md-4</div>
-            <div className="col-md-4 col-md-offset-4 team-div div-height">.col-md-4 .col-md-offset-4</div>
+            <div className="col-md-4 mission-div div-height jumbotron">
+            <div className="col-md-4 inside-div">
+              Our Mission
+            </div>
+            <div className="info-div">
+              <span className="letter"><b>L</b></span>orem ipsum dolor sit amet, consectetur adipiscing elit. Sed et felis vulputate,
+                fringilla tortor ac, mollis nisl. Donec rhoncus tristique felis, vitae condimentum
+                justo. Vestibulum congue tristique nisi vitae rutrum. Mauris mattis dapibus
+                volutpat. Mauris at iaculis nisi. Curabitur placerat lacinia ex et sodales. Vivamus
+                in condimentum est.
+              <br />
+              <br />
+              <img className="info-image" src="/img/bank.png" />
+              <br />
+              <br />
+              Nulla suscipit tempus est, id vulputate velit. Pellentesque rhoncus orci nec quam
+                gravida tincidunt. Nullam ut ipsum turpis. Phasellus porta mattis sem, a aliquet
+                elit molestie in. Etiam vitae faucibus quam. Fusce aliquet molestie commodo.
+                Suspendisse sapien quam, egestas ut est a, scelerisque dictum metus. Mauris id
+                tortor quis diam vulputate porta sed in turpis. Mauris sit amet lacus ut mauris
+                feugiat tempor. Aliquam auctor a neque in ornare. Nulla facilisi. Proin scelerisque
+                sapien turpis, sit amet blandit augue vestibulum vitae. Nulla placerat diam varius
+                tempor sodales. Maecenas vestibulum est quis enim maximus, sit amet volutpat sapien
+                cursus.
+                <br />
+                <br />
+                <img className="info-house" src="/img/House.png" />
+                <br />
+                <br />
+                Mauris nec erat a est euismod interdum. Ut vel semper erat. Nulla sed nisl semper,
+                facilisis massa vel, dignissim nulla. In tempor vulputate tellus, sed dapibus augue
+                 ultrices non.
+            </div>
+            </div>
+            <div className="col-md-4 col-md-offset-4 team-div div-height jumbotron">
+            <div className="col-md-4 inside-div">
+                Our Team
+              </div>
+            </div>
           </div>
         </div>
 
